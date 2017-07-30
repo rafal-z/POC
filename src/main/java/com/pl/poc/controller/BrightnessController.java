28e3c09a0287903fb78fdb311bf039ec706bdcd1
rafal-z <rafal.zebrowski1@gmail.com>
2017-07-30 09:46:09 +0200

    -Dodanie metody execute() do klasy ImagesAlgorithms;

--- src/main/java/com/pl/poc/controller/BrightnessController.java
+++ src/main/java/com/pl/poc/controller/BrightnessController.java
@@ -1,6 +1,7 @@
 package com.pl.poc.controller;
 
 import com.pl.poc.algorithm.ImageAlgorithms;
+import com.pl.poc.model.ImagesModel;
 import com.pl.poc.view.BrightnessSettingsView;
 import com.pl.poc.view.MainView;
 
@@ -8,7 +9,6 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferInt;
 
 /**
  * Created by Rafał on 2017-07-15.
@@ -27,7 +27,8 @@ public class BrightnessController implements ChangeListener {
         bsView.getSpiner().setValue(slider.getValue());
         try
         {
-            processImage(mView.getImagesModel().getSrcImage(), mView.getImagesModel().getDstImage(), slider.getValue());
+            ImagesModel imagesModel = ImageAlgorithms.execute(mView.getImagesModel(), slider.getValue());
+            mView.setImagesModel(imagesModel);
             mView.repaint();
         }
         catch (Exception ex)
@@ -36,22 +37,4 @@ public class BrightnessController implements ChangeListener {
         }
     }
 
-    void processImage(BufferedImage src, BufferedImage dst, int db)
-    {
-        DataBufferInt sbuff = (DataBufferInt) src.getRaster().getDataBuffer();
-        DataBufferInt dbuff = (DataBufferInt) dst.getRaster().getDataBuffer();
-        int[] sp = sbuff.getData();
-        int[] dp = dbuff.getData();
-
-        int i = 0;
-        for (int y = 0; y < src.getHeight(); ++y)
-        {
-            for (int x = 0; x < src.getWidth(); ++x)
-            {
-                dp[i] = ImageAlgorithms.changeBrightness(sp[i], db);
-                i++;
-            }
-        }
-
-    }
 }
